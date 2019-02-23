74fa097a0085b8414eaa3f289e2d4d669b4c806c
markosviggiato <markosviggiato@gmail.com>
2019-02-22 17:53:27 -0700

    commit initial structure of the project

---
+++ src/main/java/markos/messageBoard/Application.java
@@ -0,0 +1,13 @@
+package markos.messageBoard;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
