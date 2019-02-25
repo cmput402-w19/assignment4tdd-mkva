c14b6bbffd82b8dd055c30ed840d670f318d86a3
markosviggiato <markosviggiato@gmail.com>
2019-02-25 02:02:14 -0700

    RED - create Message and MessageTest classes and test valid content of message

---
+++ src/main/java/markos/messageBoard/Message.java
@@ -0,0 +1,54 @@
+package markos.messageBoard;
+
+public class Message {
+
+	private int type; //1=question and 2=answer
+	private int id;
+	private int parent;
+	private String content;
+	private String author;
+	private String date;
+
+	public Message(int type, String content, String author, String date) throws Exception {
+		
+	}
+	
+	public Message(int type, int id, int parent, String content, String author, String date) {
+
+	}
+
+	public int getId() {
+		return (Integer) null;
+	}
+
+
+	public int getParent() {
+		return (Integer) null;
+	}
+
+	public int getType() {
+		return (Integer) null;
+	}
+
+	public String getAuthor() {
+		return null;
+	}
+
+	public String getContent() {
+		return null;
+	}
+
+	public String getAtuhor() {
+		return null;
+	}
+
+	public String getDate() {
+		return null;
+	}
+
+	public boolean validContent(String content) throws Exception {
+		return (Boolean) null;
+	}
+}
+
+
---
+++ src/test/java/markos/messageBoard/MessageTest.java
@@ -0,0 +1,20 @@
+package markos.messageBoard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class MessageTest {
+
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testValidContent() throws Exception {
+		Message post = new Message(1, "This is a post content", "markosva", "2019-02-21");
+		assertTrue(post.validContent("This is a post content"));
+	}
+
+}
