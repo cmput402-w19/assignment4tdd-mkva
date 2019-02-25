c9099e430bfdf7552166d9d7962487a53b6c0e14
markosviggiato <markosviggiato@gmail.com>
2019-02-25 03:01:18 -0700

    RED - create class and test class for user management and test user login

---
+++ src/main/java/markos/messageBoard/UserManagement.java
@@ -0,0 +1,18 @@
+package markos.messageBoard;
+
+public class UserManagement {
+
+	public User userLogin() {
+		return null;
+		
+	}
+	
+	public User userRegister() {
+		return null;
+		
+	}
+
+	public void filterUsersAndDisplay() {
+
+	}
+}
---
+++ src/test/java/markos/messageBoard/UserManagementTest.java
@@ -0,0 +1,18 @@
+package markos.messageBoard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserManagementTest {
+
+	@Test
+	public void testUserLoginUsername() {
+		String username = "usernamefoo";
+		String password = "pass";
+		UserManagement userManag = new UserManagement();
+		User currentUser = userManag.userLogin();
+		assert(currentUser.getUsername().equals("usernamefoo"));
+	}
+
+}
