f7c0f7df634e31da0acce74bfe0d416ceb4429da
markosviggiato <markosviggiato@gmail.com>
2019-02-25 02:06:51 -0700

    RED - test empty content

--- src/test/java/markos/messageBoard/MessageTest.java
+++ src/test/java/markos/messageBoard/MessageTest.java
@@ -9,11 +9,18 @@ import org.junit.rules.ExpectedException;
 public class MessageTest {
 
 	@Rule
-    public ExpectedException thrown = ExpectedException.none();
+	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
 	public void testValidContent() throws Exception {
 		Message post = new Message(1, "This is a post content", "markosva", "2019-02-21");
 		assertTrue(post.validContent("This is a post content"));
+	}
+
+	@Test
+	public void testValidContentEmpty() throws Exception {
+		thrown.expect(Exception.class);
+		thrown.expectMessage("Invalid post! Posts must have a content.");
+		Message post = new Message(1, "", "markosva", "2019-02-21");
 	}
 }
