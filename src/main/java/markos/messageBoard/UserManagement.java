8ba6d9766754eeb129f9e041003241b904f824e6
markosviggiato <markosviggiato@gmail.com>
2019-02-25 14:14:35 -0700

    RED - test filter users by star

--- src/main/java/markos/messageBoard/UserManagement.java
+++ src/main/java/markos/messageBoard/UserManagement.java
@@ -1,7 +1,11 @@
 package markos.messageBoard;
 
+import services.UserInfo;
+
 public class UserManagement {
 
+	private int filter;
+	
 	public User userLogin() throws Exception {
 		User user = new User("usernamefoo", "nameX", "usernamefoo@email.com");
 		return user;
@@ -13,7 +17,11 @@ public class UserManagement {
 		
 	}
 
-	public void filterUsersAndDisplay() {
+	public void filterUsersAndDisplay(UserInfo userInfo) {
 
+	}
+	
+	public int getFilter() {
+		return (Integer) null;
 	}
 }
--- src/test/java/markos/messageBoard/UserManagementTest.java
+++ src/test/java/markos/messageBoard/UserManagementTest.java
@@ -7,7 +7,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import services.UserInfo;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserManagementTest {
 
@@ -17,6 +25,25 @@ public class UserManagementTest {
 		UserManagement userManag = new UserManagement();
 		User currentUser = userManag.userLogin();
 		assert(currentUser.getUsername().equals("usernamefoo"));
+	}
+	
+	@Test
+	public void testFilterUsers() throws Exception {
+		UserManagement userManag = new UserManagement();
+		UserInfo userMock = mock(UserInfo.class); 
+		
+		// create users
+		List<User> listUsersAll = new ArrayList<User>();
+		User u1 = new User("username1", "name1", 0);
+		User u2 = new User("username2", "name2", 2);
+		listUsersAll.add(u1);
+		listUsersAll.add(u2);
+		
+		when(userMock.listUsers()).thenReturn(listUsersAll);
+		userManag.filterUsersAndDisplay(userMock);
+		
+    	assert(1 == userManag.getFilter());
+    	verify(userMock, times(1)).listUsers();
 	}
 
 }
