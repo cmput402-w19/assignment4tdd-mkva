f1200aa82bc99d40a1f2dc9d95bb6b1a28351426
markosviggiato <markosviggiato@gmail.com>
2019-02-25 13:31:57 -0700

    fix travis ci integration with mysql

--- src/main/java/db/DAOAccess.java
+++ src/main/java/db/DAOAccess.java
@@ -19,7 +19,7 @@ public class DAOAccess {
 	public void initialize() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		// Setup the connection with the DB
-		connect = DriverManager.getConnection("jdbc:mysql://localhost/messageBoard", "root", "markosalberta");
+		connect = DriverManager.getConnection("jdbc:mysql://localhost/messageBoard", "root", "");
 	}
 
 	public Connection getAllMessages() throws Exception {
