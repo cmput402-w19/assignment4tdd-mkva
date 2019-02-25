ef6d671a9071edce6f848272b73bee121cfd7eb2
markosviggiato <markosviggiato@gmail.com>
2019-02-25 01:07:32 -0700

    RED - test empty username

--- src/test/java/markos/messageBoard/UserTest.java
+++ src/test/java/markos/messageBoard/UserTest.java
@@ -2,7 +2,9 @@ package markos.messageBoard;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class UserTest {
 	//TEST CASES FOR USERNAME VALIDITY
@@ -10,6 +12,17 @@ public class UserTest {
 	public void testValidUsername() throws Exception {
 		User user = new User("usernamefoo", "My Namefoo", "MyEmailfoo@email.com");
 		assertTrue(user.validUsername("usernamefoo"));
+	}
+	
+	// boundary test - size = 0 (error)
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testValidUsernameSizeZero() throws Exception {
+		thrown.expect(Exception.class);
+	    thrown.expectMessage("Invalid username! Usernames cannot be empty.");
+	    User user = new User("", "My Namefoo", "MyEmailfoo@email.com");
 	}
 
 }
