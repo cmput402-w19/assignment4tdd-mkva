0a0ec01cc64ce2d4506df513d73a14055a239225
markosviggiato <markosviggiato@gmail.com>
2019-02-25 10:43:22 -0700

    RED - test get all messages

--- src/test/java/db/DAOAccessTest.java
+++ src/test/java/db/DAOAccessTest.java
@@ -2,13 +2,30 @@ package db;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.mysql.cj.xdevapi.Statement;
 
 public class DAOAccessTest {
 
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
 	@Test
-	public void test() {
+	public void testGetAllMessagesWorking() {
+		DAOAccess db = new DAOAccess();
+		db.getAllMessages();
 		
+		//check if connect is filled
+		assertTrue(connect!=null);
 	}
 
 }
--- src/test/java/markos/messageBoard/UserManagementTest.java
+++ src/test/java/markos/messageBoard/UserManagementTest.java
@@ -2,9 +2,12 @@ package markos.messageBoard;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+
 
 public class UserManagementTest {
 
