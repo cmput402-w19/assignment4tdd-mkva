10a6f69260fdd3a36127acb31a8d974c42a514ef
markosviggiato <markosviggiato@gmail.com>
2019-02-25 12:40:46 -0700

    REFACTOR and GREEN - final test for get all messages

--- src/test/java/db/DAOAccessTest.java
+++ src/test/java/db/DAOAccessTest.java
@@ -8,9 +8,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.mysql.cj.xdevapi.Statement;
+
+import db.DAOAccess;
 
 public class DAOAccessTest {
 
@@ -18,7 +22,7 @@ public class DAOAccessTest {
 	private Statement statement = null;
 	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
-	
+
 	@Test
 	public void testGetAllMessagesWorking() throws Exception {
 		try {
@@ -26,11 +30,20 @@ public class DAOAccessTest {
 			connect = db.getAllMessages();	
 			//check if connect is filled
 			assertTrue(connect!=null);
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	@Rule
+	public ExpectedException thrown= ExpectedException.none();
+
+	// check if no exception is thrown
+	@Test
+	public void throwsNothing() throws Exception {
+		DAOAccess db = new DAOAccess();
+		connect = db.getAllMessages();
 	}
 
 }
