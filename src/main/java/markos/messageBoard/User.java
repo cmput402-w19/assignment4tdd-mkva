7ca42b7695bac0c25b703ac42d9ce8131bccecf0
markosviggiato <markosviggiato@gmail.com>
2019-02-25 01:15:22 -0700

    GREEN - test username with special characters

--- src/main/java/markos/messageBoard/User.java
+++ src/main/java/markos/messageBoard/User.java
@@ -1,5 +1,8 @@
 package markos.messageBoard;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class User {
 
 	private String username;
@@ -25,9 +28,19 @@ public class User {
 
 	public boolean validUsername(String username) throws Exception {
 		Exception emptyUsername = new Exception("\nInvalid username! Usernames cannot be empty.");
+		Exception specialCharactersyUsername = new Exception("Invalid username! Usernames cannot"
+				+ " have special characters.");
 		// check if username is empty
 		if(username.isEmpty())
-			throw emptyUsername;		
+			throw emptyUsername;
+
+		// check if username contains special characters
+		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(username);
+		boolean containSpecialCharacter = m.find();
+		if(containSpecialCharacter)
+			throw specialCharactersyUsername;
+
 		return true;
 	}
 
