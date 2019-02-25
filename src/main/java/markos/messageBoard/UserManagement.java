8e6cf2857f8f2e3d893149598397e587443f4843
markosviggiato <markosviggiato@gmail.com>
2019-02-25 14:53:52 -0700

    REFACTOR - fix identation and remove some comments

--- src/main/java/markos/messageBoard/UserManagement.java
+++ src/main/java/markos/messageBoard/UserManagement.java
@@ -8,16 +8,16 @@ import services.UserInfo;
 public class UserManagement {
 
 	private int filter;
-	
+
 	public User userLogin() throws Exception {
 		User user = new User("usernamefoo", "nameX", "usernamefoo@email.com");
 		return user;
-		
+
 	}
-	
+
 	public User userRegister() {
 		return null;
-		
+
 	}
 
 	public void filterUsersAndDisplay(UserInfo userInfo) throws Exception {
@@ -25,43 +25,33 @@ public class UserManagement {
 		Scanner userOk = new Scanner(System.in);
 		List<User> allUsers = userInfo.listUsers();
 		System.out.println("\nAll current users with more than one star.\n");
-    	System.out.println("\n--------------------------------------------------------------------");
-    	System.out.println("\n--------------------------------------------------------------------\n");
-    	boolean noUserWithStars = true;
-    	filter=0;
-    	
-    	if(allUsers.isEmpty()) {
-    		System.out.println("\nThere is no registered user.\n");
-    		Thread.sleep(3000);
-    	}
-    		
+		System.out.println("\n--------------------------------------------------------------------");
+		System.out.println("\n--------------------------------------------------------------------\n");
+		boolean noUserWithStars = true;
+		filter=0;
+
+		if(allUsers.isEmpty()) {
+			System.out.println("\nThere is no registered user.\n");
+			Thread.sleep(3000);
+		}
+
 		for (User user:allUsers) {
 			if(user.getStars() > 0 ) {
 				filter++;
 				noUserWithStars = false;
 				System.out.println("Username: " + user.getUsername() + "\n");
-		        System.out.println("Name : " + user.getName() + "\n");
-		        System.out.println("Number of stars : " + user.getStars() + "\n");
-		        System.out.println("\n--------------------------------------------------------------------\n");
+				System.out.println("Name : " + user.getName() + "\n");
+				System.out.println("Number of stars : " + user.getStars() + "\n");
+				System.out.println("\n--------------------------------------------------------------------\n");
 			}
 		}
-		
+
 		if(noUserWithStars) {
 			System.out.println("\nThere is no user with more than 1 star.\n");
 			Thread.sleep(3000);
 		}
-			
-		
-//		System.out.println("\nPlease enter 'ok' to return.\n");
-//		while(true) {
-//			String userReturn = userOk.nextLine();
-//			if(userReturn.toLowerCase().equals("ok"))
-//				return;
-//			else
-//				System.out.println("\nPlease enter the suggested command.\n");
-//		}
 	}
-	
+
 	public int getFilter() {
 		return this.filter;
 	}
--- src/test/java/markos/messageBoard/UserManagementTest.java
+++ src/test/java/markos/messageBoard/UserManagementTest.java
@@ -26,24 +26,24 @@ public class UserManagementTest {
 		User currentUser = userManag.userLogin();
 		assert(currentUser.getUsername().equals("usernamefoo"));
 	}
-	
+
 	@Test
 	public void testFilterUsers() throws Exception {
 		UserManagement userManag = new UserManagement();
 		UserInfo userMock = mock(UserInfo.class); 
-		
+
 		// create users
 		List<User> listUsersAll = new ArrayList<User>();
 		User u1 = new User("username1", "name1", 0);
 		User u2 = new User("username2", "name2", 2);
 		listUsersAll.add(u1);
 		listUsersAll.add(u2);
-		
+
 		when(userMock.listUsers()).thenReturn(listUsersAll);
 		userManag.filterUsersAndDisplay(userMock);
-		
-    	assert(1 == userManag.getFilter());
-    	verify(userMock, times(1)).listUsers();
+
+		assert(1 == userManag.getFilter());
+		verify(userMock, times(1)).listUsers();
 	}
 
 }
