9725b6d9c93eb3fbb7f1bf9172f60b5ac503cb15
markosviggiato <markosviggiato@gmail.com>
2019-02-25 09:42:35 -0700

    create class to access database

--- src/test/java/markos/messageBoard/UserManagementTest.java
+++ src/test/java/markos/messageBoard/UserManagementTest.java
@@ -2,14 +2,15 @@ package markos.messageBoard;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class UserManagementTest {
 
 	@Test
 	public void testUserLoginUsername() throws Exception {
 		String username = "usernamefoo";
-		String password = "pass";
 		UserManagement userManag = new UserManagement();
 		User currentUser = userManag.userLogin();
 		assert(currentUser.getUsername().equals("usernamefoo"));
