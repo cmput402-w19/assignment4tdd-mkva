9492da13846937b687eb041c5c8aa78039fa6aab
markosviggiato <markosviggiato@gmail.com>
2019-02-25 17:52:45 -0700

    GREEN - test message filter

--- src/main/java/markos/messageBoard/MessageManagement.java
+++ src/main/java/markos/messageBoard/MessageManagement.java
@@ -1,5 +1,6 @@
 package markos.messageBoard;
 
+import java.util.List;
 import java.util.Scanner;
 
 import db.DAOAccess;
@@ -27,12 +28,38 @@ public class MessageManagement {
 
 	}
 
-	public void filterMessagesAndDisplay(MessageInfo messageInfo) {
+	public void filterMessagesAndDisplay(MessageInfo messageInfo) throws Exception {
+		Scanner userOk = new Scanner(System.in);
+		List<Message> allMessages = messageInfo.listAllMessages();
 		
+		System.out.println("\nAll current messages that are answers to other messages.\n");
+    	System.out.println("\n--------------------------------------------------------------------");
+    	System.out.println("\n--------------------------------------------------------------------\n");
+    	boolean noMessagesAnswers = true;
+    	
+    	filter=0;
+    	if(allMessages.isEmpty())
+    		System.out.println("\nThere is no message.\n");
+		for (Message mess:allMessages) {
+			if(mess.getType() == 2) {
+				filter++;
+				noMessagesAnswers = false;
+				System.out.println("ID : " + mess.getId() + " (Answer to message " + mess.getParent() +  ")\n");
+				System.out.println("Message: " + mess.getContent() + "\n");
+		        System.out.println("Author : " + mess.getAuthor() + "\n");
+		        System.out.println("Date : " + mess.getDate() + "\n");
+		        System.out.println("\n--------------------------------------------------------------------\n");
+			}
+		}
+		
+		if(noMessagesAnswers) {
+			System.out.println("\nThere is no message which is answer.\n");
+			Thread.sleep(3000);
+		}
 	}
 	
 	public int getFilter() {
-		return (Integer) null;
+		return this.filter;
 	}
 
 }
